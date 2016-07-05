f9ac814410dc688c3766155db9126f808b9db2e5
kenpcjr <kennethcooke@me.com>
2016-07-05 12:54:11 -0400

    Done.

---
+++ PrintKC.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var favoriteColor = "Orange"
+
+var favoriteWord = "Haberdashery"
+
+var favorite2000sEraBand = "Coheed and Cambria"
+
+print(favoriteWord)
+print(favoriteColor);
+print(favorite2000sEraBand)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+print("Hi Mom! My favorite words is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite 2000's era band is \(favorite2000sEraBand).")
+
+var howEasy = "very easy"
+
+var swiftIs = "Swift is \(howEasy)!"
+
+print(swiftIs)
+
+print(swiftIs + " So far.")
